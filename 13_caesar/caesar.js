c0be93ac1473f533c217423897a2c9af4f83c9a1
Vladislav Kosovsky <kosovsky.vladislav@gmail.com>
2023-03-21 13:55:21 +0300

    Initial commit

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,28 +1,20 @@
-const caesar = function (str, num) {
-  return str
+function caesar(phrase, shift) {
+  return phrase
     .split("")
     .map((letter) => {
-      const charCode = letter.charCodeAt(0) + num;
-      if (/\W/g.test(letter)) {
+      const charCode = letter.charCodeAt();
+      const basePosition = charCode < 97 ? 65 : 97;
+      const shiftedCode =
+        ((((charCode + shift - basePosition) % 26) + 26) % 26) + basePosition;
+      if (/[\W]/g.test(letter)) {
         return letter;
       } else {
-        if (letter.charCodeAt(0) >= 97 && charCode >= 65 && charCode <= 90) {
-          return String.fromCharCode(charCode + 26);
-        } else {
-          return (charCode >= 65 && charCode <= 90) ||
-            (charCode >= 97 && charCode <= 122)
-            ? String.fromCharCode(charCode)
-            : charCode < 65 || (charCode > 122 && charCode <= 127)
-            ? String.fromCharCode(charCode + 26)
-            : charCode > 127
-            ? String.fromCharCode(charCode - 78)
-            : String.fromCharCode(charCode - 26);
-        }
+        return String.fromCharCode(shiftedCode);
       }
     })
     .join("");
-};
+}
 console.log("Hello, World!".charCodeAt(1) - 29);
-console.log(caesar("Hello, World!", -29));
+console.log(caesar("conquer et impera", 13));
 // Do not edit below this line
 module.exports = caesar;
